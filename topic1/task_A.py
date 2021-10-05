9bcb389bf1a4bda8ebfef73655cc803eee735b8b
Valeriya Lakshina <lakshinav@users.noreply.github.com>
2021-10-05 23:15:54 +0300

    Update task_A.py

--- topic1/task_A.py
+++ topic1/task_A.py
@@ -1,15 +1,23 @@
-# A.Кондиционер
+# A.Interactor
 
-troom, tcond = tuple(map(int, input().split()))
-mode = input()
+def Interactor(returncode, interactor, checker):
+    if interactor == 0 and returncode != 0:
+        return 3
+    if interactor == 0 and returncode == 0:
+        return checker
+    if interactor == 1:
+        return checker
+    if interactor == 4 and returncode != 0:
+        return 3
+    if interactor == 4 and returncode == 0:
+        return 4
+    if interactor == 6:
+        return 0
+    if interactor == 7:
+        return 1
+    return interactor
 
-if mode == 'fan':
-    print(troom)
-elif mode == 'auto':
-   print(tcond)
-elif mode == 'freeze' and tcond < troom:
-   print(tcond)
-elif mode == 'heat' and tcond > troom:
-   print(tcond)
-else:
-   print(troom)
+returncode = int(input())
+interactor = int(input())
+checker = int(input())
+print(Interactor(returncode, interactor, checker))
