d58ec6ab8b802d7e403327b1af9cbb27058e03e6
Valeriya Lakshina <lakshinav@users.noreply.github.com>
2021-10-05 23:17:14 +0300

    Update task_B.py

--- topic1/task_B.py
+++ topic1/task_B.py
@@ -1,12 +1,9 @@
-# B. Треугольник
+# B. Кольцевая линия метро
 
-# хоть и сортируем, но сортируемых элементов всегда 3, поэтому O(1)
+def min_stations_cnt(n,i,j):
+    return min(
+        abs(i-j)-1,
+        n-max(i,j) + min(i,j) - 1
+    )
 
-tri = []
-for i in range(3):
-    tri.append(int(input()))
-tri.sort()
-if tri[0]+tri[1] > tri[2]:
-    print('YES')
-else:
-    print('NO')
+print(min_stations_cnt(*tuple(map(int, input().split()))))
